91d53ce207707f4dddfa137f80543993ce638eea
Hyungchan Kim <inlinechan@gmail.com>
2016-12-26 16:10:55 +0900

    R: Expectation as vector

--- tests/IndexerTest.cpp
+++ tests/IndexerTest.cpp
@@ -4,6 +4,8 @@
 #include "Indexer.h"
 #include "IndexerArchive.h"
 
+#include <vector>
+
 using ::testing::_;
 using ::testing::AtLeast;
 
@@ -19,38 +21,45 @@ public:
 
 class IndexerTest : public ::testing::Test {
 protected:
-  void SetUp() override {
-  }
+  void SetUp() override {}
 
-  void TearDown() override {
-  }
+  void TearDown() override {}
 };
 
 TEST_F(IndexerTest, Basic) {
+  struct Expectation {
+    std::string fileName;
+    unsigned beginOffset;
+    std::string functionName;
+    ReferenceType type;
+  };
+
+  const std::string fileName(XSTRINGIFY(TEST_BASEDIR) "/basic/basic.cpp");
+  const std::string increment("increment");
+  const std::string add("add");
+  const std::string main("main");
+
+  std::vector<Expectation> expectations = {
+      {fileName, 4, increment, ReferenceType::RT_FUNCTION_DECL},
+      {fileName, 26, add, ReferenceType::RT_FUNCTION_DECL},
+      {fileName, 52, main, ReferenceType::RT_FUNCTION_DEFI},
+      {fileName, 126, add, ReferenceType::RT_FUNCTION_REF},
+      {fileName, 146, increment, ReferenceType::RT_FUNCTION_REF},
+      {fileName, 181, increment, ReferenceType::RT_FUNCTION_DEFI},
+      {fileName, 223, add, ReferenceType::RT_FUNCTION_DEFI}};
+
   MockIndexerArchive archive;
 
-  // /home/hyungchan/source/c++/finder/tests/basic/basic.cpp:4:increment RT_FUNCTION_DECL
-  // /home/hyungchan/source/c++/finder/tests/basic/basic.cpp:26:add RT_FUNCTION_DECL
-  // /home/hyungchan/source/c++/finder/tests/basic/basic.cpp:52:main RT_FUNCTION_DEFI
-  // /home/hyungchan/source/c++/finder/tests/basic/basic.cpp:126:add RT_FUNCTION_REF
-  // /home/hyungchan/source/c++/finder/tests/basic/basic.cpp:146:increment RT_FUNCTION_REF
-  // /home/hyungchan/source/c++/finder/tests/basic/basic.cpp:181:increment RT_FUNCTION_DEFI
-  // /home/hyungchan/source/c++/finder/tests/basic/basic.cpp:223:add RT_FUNCTION_DEFI
-  const std::string fileName(XSTRINGIFY(TEST_BASEDIR) "/basic/basic.cpp");
-  // EXPECT_CALL(archive, recordRef(fileName, _, _, _)).Times(7);
-  EXPECT_CALL(archive, recordRef(fileName, 223, "add", ReferenceType::RT_FUNCTION_DEFI));
-  EXPECT_CALL(archive, recordRef(fileName, 181, "increment", ReferenceType::RT_FUNCTION_DEFI));
-  EXPECT_CALL(archive, recordRef(fileName, 146, "increment", ReferenceType::RT_FUNCTION_REF));
-  EXPECT_CALL(archive, recordRef(fileName, 126, "add", ReferenceType::RT_FUNCTION_REF));
-  EXPECT_CALL(archive, recordRef(fileName, 52, "main", ReferenceType::RT_FUNCTION_DEFI));
-  EXPECT_CALL(archive, recordRef(fileName, 26, "add", ReferenceType::RT_FUNCTION_DECL));
-  EXPECT_CALL(archive, recordRef(fileName, 4, "increment", ReferenceType::RT_FUNCTION_DECL));
+  for (auto it = expectations.rbegin(); it != expectations.rend(); ++it) {
+    auto e = *it;
+    EXPECT_CALL(archive,
+                recordRef(e.fileName, e.beginOffset, e.functionName, e.type));
+  }
 
   const char *Argv[] = {
-    "runUnitTests",
-    XSTRINGIFY(TEST_BASEDIR) "/basic/basic.cpp",
+      "runUnitTests", XSTRINGIFY(TEST_BASEDIR) "/basic/basic.cpp",
   };
-  int Argc = sizeof(Argv) / sizeof(char*);
+  int Argc = sizeof(Argv) / sizeof(char *);
   Indexer indexer(Argc, Argv, archive);
   indexer.index();
 }
