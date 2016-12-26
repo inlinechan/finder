843500e6b07984b08549c35093268e80ea19d48c
Hyungchan Kim <inlinechan@gmail.com>
2016-12-26 11:29:57 +0900

    R: Move #include to cpp
    + #ifndef INDEXER_H ~ #endif

--- src/Indexer.cpp
+++ src/Indexer.cpp
@@ -1,5 +1,26 @@
 #include "Indexer.h"
 
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Driver/Options.h"
+#include "clang/Frontend/ASTConsumers.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Rewrite/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/TargetSelect.h"
+
+#include <string>
+#include <vector>
+
+using namespace clang::driver;
+using namespace clang::tooling;
+
 enum class ReferenceType {
   RT_FUNCTION_DECL,
   RT_FUNCTION_DEFI,
@@ -41,7 +62,6 @@ private:
   clang::Preprocessor &Preprocessor;
   IndexerArchive &Archive;
 };
-
 
 class IndexerASTConsumer : public clang::ASTConsumer {
 public:
